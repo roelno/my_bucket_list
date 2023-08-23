979f4251de53647d06f4d1c515263e8646180572
Ro <lichtseeker@gmail.com>
2023-08-22 23:17:00 -0700

    Setup the MainActivity: set OnclickListener for two card views

--- app/src/main/java/com/zelda/mybucketlist/MainActivity.java
+++ app/src/main/java/com/zelda/mybucketlist/MainActivity.java
@@ -1,14 +1,41 @@
 package com.zelda.mybucketlist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    CardView thingsToDo;
+    CardView placeToGo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // find view
+        thingsToDo = findViewById(R.id.card_view_things);
+        placeToGo = findViewById(R.id.card_view_places);
+
+        thingsToDo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent thingsIntent = new Intent(MainActivity.this, ThingsActivity.class);
+                startActivity(thingsIntent);
+            }
+        });
+
+        placeToGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent placesIntent = new Intent(MainActivity.this, PlacesActivity.class);
+                startActivity(placesIntent);
+            }
+        });
+
     }
 }
